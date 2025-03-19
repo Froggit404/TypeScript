435a47b20b59d109c4fa8a68f4ec2c041d6827f4
Froggit404 <hrytsykdiplom@gmail.com>
2025-03-20 00:31:21 +0100

    Почав виконувати ДЗ

---
+++ Lesson-1/HW-1.ts
@@ -0,0 +1,21 @@
+{
+    let a: string = 'hello'
+    let b: string = 'owu'
+    let c: string = 'com'
+    let d: string = 'ua'
+    let e: number = 1
+    let f: number = 10
+    let g: number = -999
+    let h: number = 123
+    let i: number = 3.14
+    let j: number = 2.7
+    let k: number = 16
+    let l: boolean = true
+    let m: boolean = false
+
+    console.log(a, b, c, d, e, f, g, h, i, j, k, l, m)
+}
+// Створити змінні. Присвоїти кожному з них значення: ‘hello’,’owu’,’com’, ‘ua’, 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// Вивести кожну змінну за допомогою: console.log
+
+
